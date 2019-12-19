eb4387efc739c33d05e1d443ebc4a64d6fdc93b2
Sforzemon <sforzemon@gmail.com>
2019-12-19 18:31:27 -0500

    Just adding a few html and css aspects.

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Which jQuery method is used to switch between adding/removing one or more classes (for CSS) from selected elements?",
+        choices: ["toggleClass()", "switch()", "altClass()", "switchClass()"],
+        answer: "switchClass()"
+    },
+    {
+        title: 'What is the correct JavaScript syntax to write "Hello World"?',
+        choices: ['document.write("Hello World")', '("Hello World")', 'response.write("Hello World")', '"Hello World"'],
+        answer: 'document.write("Hello World")'
+    },
+    {
+        title: "jQuery is a...",
+        choices: ["Ruby Gem", "JavaScript Library", "PHP Framework", "None of the above"],
+        answer: "JavaScript library"
+    },
+  ];
